8d642dc0d2148786b8a95f9b7db8339a526de4a8
Pavlo Bessmertnyy <pavlo.bessmertnyy@globallogic.com>
2020-09-01 18:23:00 +0300

    Updates

---
+++ precache-manifest.d28b41210d614c9acb3dc9ce911b2a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a57ab274db1437e9f9de606a77377cb",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fa49078c2abb0776eb90",
+    "url": "/react-hw-01-components/static/css/main.c9c2d086.chunk.css"
+  },
+  {
+    "revision": "e994a5b3a5ee7dcb5b4b",
+    "url": "/react-hw-01-components/static/js/2.2e37d07e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.2e37d07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa49078c2abb0776eb90",
+    "url": "/react-hw-01-components/static/js/main.2c005d0c.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
