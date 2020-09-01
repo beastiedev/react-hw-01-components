e1e00ec896d431d00aa2c1f5a3002b8e1986a822
Pavlo Bessmertnyy <pavlo.bessmertnyy@globallogic.com>
2020-09-01 17:31:23 +0300

    Add Transaction list component and styles

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import Statistics from './components/Statistics';
 import statisticalData from './data/statistical-data.json';
 import FriendList from './components/FriendList';
 import friendsData from './data/friends.json';
+import TransactionHistory from './components/TransactionHistory';
+import transactionData from './data/transactions.json';
 
 function App() {
   return (
     <div className="App">
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} />
-      <Statistics
-        title="Upload stats"
-        stats={statisticalData} />
-      <FriendList friends={friendsData} />
+      <div className="wrap">
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats} />
+        <Statistics
+          title="Upload stats"
+          stats={statisticalData} />
+      </div>
+      <div className="wrap">
+        <FriendList friends={friendsData} />
+      </div>
+      <div className="wrap wrap-out">
+        <TransactionHistory items={transactionData} />
+      </div>
+
     </div>
   );
 }
